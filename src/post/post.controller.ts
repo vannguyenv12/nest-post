ac631e47fda680416f9424d24bfda6f1988082c7
vannguyenv12 <npnv.vn1@gmail.com>
2024-10-28 21:28:14 +0700

    feat: post body

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { PostDocument } from './schemas/post.schema';
 
 @Controller('posts') // REST
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create() {
-    return this.postService.create();
+  create(@Body() requestBody: PostDocument) {
+    return this.postService.create(requestBody);
   }
 }
+
+// DTO
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Post } from './schemas/post.schema';
+import { Post, PostDocument } from './schemas/post.schema';
 
 @Injectable()
 export class PostService {
-  constructor(@InjectModel(Post.name) private postModal: Model<Post>) {}
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
 
-  create() {
-    const createdPost = new this.postModal({
-      title: 'Post 1',
-      description: 'Description 1',
-    });
+  create(data: PostDocument) {
+    const createdPost = new this.postModel(data);
 
     return createdPost.save();
   }
